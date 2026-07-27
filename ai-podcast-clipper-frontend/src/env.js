6081588b3acb320f52fcf63805dc855a5c7253c4
Ankit Kumar <ankitkumar.iitp09@gmail.com>
2026-07-27 22:48:43 +0530

    deployment err

--- ai-podcast-clipper-frontend/src/env.js
+++ ai-podcast-clipper-frontend/src/env.js
@@ -49,7 +49,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     AWS_ACCESS_KEY_ID: process.env.PODSNAP_AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.PODSNAP_AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY,
-    AWS_REGION: process.env.PROCESS_AWS_REGION || process.env.AWS_REGION,
+    AWS_REGION: process.env.PODSNAP_AWS_REGION || process.env.AWS_REGION,
     S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
     PROCESS_VIDEO_ENDPOINT: process.env.PROCESS_VIDEO_ENDPOINT,
     PROCESS_VIDEO_ENDPOINT_AUTH: process.env.PROCESS_VIDEO_ENDPOINT_AUTH,
